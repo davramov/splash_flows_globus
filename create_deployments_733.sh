66624f275ef61352ea6ded40cd68c476033f6044
David Abramov <dabramov@lbl.gov>
2025-07-07 11:47:50 -0700

    Updating prefect deployments... although I should make sure this works with Prefect3.x 🤔

--- create_deployments_733.sh
+++ create_deployments_733.sh
@@ -6,11 +6,13 @@ prefect work-pool create 'dispatcher_733_flow_pool'
 prefect work-pool create 'new_file_733_flow_pool'
 # prefect work-pool create 'new_file_733_prune_pool'
 
+
 # dispatcher_733_flow_pool
     # in docker-compose.yaml:
     # command: prefect agent start --pool "dispatcher_733_flow_pool"
 prefect deployment build ./orchestration/flows/bl733/dispatcher.py:dispatcher -n run_733_dispatcher -p dispatcher_733_pool -q dispatcher_733_queue
 prefect deployment apply dispatcher-deployment.yaml
+
 
 # new_file_733_flow_pool
     # in docker-compose.yaml:
@@ -20,9 +22,9 @@ prefect deployment apply process_new_733_file-deployment.yaml
 
 
 # TODO: Wait for PR #62 to be merged and use the new prune_controller
-# new_file_733_prune_pool
+new_file_733_prune_pool
     # in docker-compose.yaml:
     # command: prefect agent start --pool "new_file_733_prune_pool"
 
-# prefect deployment build ./orchestration/flows/bl733/prune.py:prune_data733 -n prune_data733 -p new_file_733_prune_pool -q prune_data733_queue
-# prefect deployment apply prune_data733-deployment.yaml
+prefect deployment build ./orchestration/flows/bl733/move.py:prune -n prune_data733 -p new_file_733_prune_pool -q prune_data733_queue
+prefect deployment apply prune_data733-deployment.yaml
